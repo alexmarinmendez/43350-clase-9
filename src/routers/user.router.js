76082f83d1c0ad868c89ec0c9ba13be21e5994a8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-01 20:15:24 -0500

    feat: logica en handlebars

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -6,10 +6,27 @@ const users = [
     { id: 3, name: 'Alezhia', role: 'student' },
 ]
 
+const foods = [
+    { id: 1, name: 'Banana' },
+    { id: 2, name: 'Grapes' },
+    { id: 3, name: 'Beer' },
+]
+
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.json({ users })
+    // res.json({ users })
+    res.render('showUsers', {styles: 'blue.css', users})
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    const user = users.find(item => item.id == id)
+    res.render('showUser', {
+        user,
+        isTeacher: user.role == 'teacher' ? true : false,
+        foods
+    })
 })
 
 export default router
